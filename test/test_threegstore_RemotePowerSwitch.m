0c61d33964ef3b67e52ed236b443e683d2f07618
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2018-04-06 16:14:25 -0700

    refactor

--- test/test_threegstore_RemotePowerSwitch.m
+++ test/test_threegstore_RemotePowerSwitch.m
@@ -12,5 +12,11 @@ device = threegstore.RemotePowerSwitch(...
     'cHost', cHost ...
 );
 
-device.turnOn()
-device.isOn()
+device.turnOn(1)
+device.isOn(1)
+device.turnOff(1)
+device.isOn(1)
+device.turnOn(2)
+device.isOn(2)
+device.turnOff(2)
+device.isOn(2)
