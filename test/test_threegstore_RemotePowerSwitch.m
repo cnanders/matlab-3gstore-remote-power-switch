36922deea9bd6795b00be1f5ec5db4edaf91bb2d
Chris Anderson from MET5Workstation <cnanderson@lbl.gov>
2018-04-06 16:08:52 -0700

    mods for functional testing

--- test/test_threegstore_RemotePowerSwitch.m
+++ test/test_threegstore_RemotePowerSwitch.m
@@ -6,10 +6,11 @@ addpath(genpath(fullfile(cDirThis, '..', 'src')));
 clear
 clc
 
-cHost = '192.168.10.35';
+cHost = '192.168.10.30';
 
 device = threegstore.RemotePowerSwitch(...
     'cHost', cHost ...
 );
 
-device.turnOn();
+device.turnOn()
+device.isOn()
